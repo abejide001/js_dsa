df89aecc3e2b9bf935f5944bca4f32c4a2e62541
abejide001 <abejidefemi1@gmail.com>
2020-08-11 15:42:12 +0000

    --no-edit

--- doubleLinkedList.js
+++ doubleLinkedList.js
@@ -21,4 +21,35 @@ block implements the doubly linked list node:
  }
 
  // Insertion at the head
+ // if the head of the linked list os empty, the head and tail are set to the new node
+ DoubleLinkedList.prototype.addAtFront = function (value) {
+    if (this.head === null) {
+       this.head = new DoubleLinkedListNode(value) // if the head is null
+       this.tail = this.head // we set the tail to the head
+    } else {
+       var temp = new DoubleLinkedListNode(value)
+       temp.next = this.head
+       this.head.prev = temp
+       this.head = temp
+    }
+    this.size++
+ }
+
+DoubleLinkedList.prototype.addAtTail = function (value) {
+   if (this.tail === null) {
+      this.tail = new DoubleLinkedListNode(value)
+      this.head = this.tail
+   } else {
+      var temp = new DoubleLinkedListNode(value)
+      temp.prev = this.tail
+      temp.next = temp
+      this.tail = temp
+   }
+}
  
+var dll1 = new DoubleLinkedList()
+dll1.addAtFront(20)
+dll1.addAtFront(12)
+dll1.addAtFront(1)
+dll1.addAtTail(100)
+console.log(dll1)
